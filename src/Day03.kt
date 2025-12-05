ebf7c21d1ce3d7a273c901e4a6cc489b107d57ff
Steve Malsam <stevemalsam@stevemalsam.com>
2025-12-04 17:37:09 -0800

    Day 3 pt 2

--- src/Day03.kt
+++ src/Day03.kt
@@ -7,8 +7,8 @@ fun main() {
             var rightIndex = 1
             var largestCharge = 0
 
-            while(leftIndex != batteryBank.length - 1) {
-                while(rightIndex != batteryBank.length) {
+            while(leftIndex < batteryBank.lastIndex) {
+                while(rightIndex <= batteryBank.lastIndex) {
                     val potentialBattery = "${batteryBank[leftIndex]}${batteryBank[rightIndex]}".toInt()
 
                     if(potentialBattery > largestCharge) {
@@ -28,9 +28,7 @@ fun main() {
         return outputVoltage
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+
 
     // Test if implementation meets criteria from the description, like:
 //    check(part1(listOf("test_input")) == 1)
@@ -38,9 +36,43 @@ fun main() {
     // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day03_test")
     check(part1(testInput) == 357)
+    check(part2(testInput) == 3121910778619L)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day03")
     part1(input).println()
     part2(input).println()
+}
+
+data class DigitAtIndex(val digit: Char, val index: Int)
+
+// Each line has 100 digits in it
+fun part2(input: List<String>): Long {
+    var outputJoltage: Long = 0
+    for(line in input) {
+        var lastIndex = 0
+        var numberString = ""
+        for(i in 12 downTo 1) {
+            val biggestDigit = findLargestDigitWithAtLeastNDigitsRemaining(line, lastIndex, i-1)
+            numberString += biggestDigit.digit
+            lastIndex = biggestDigit.index + 1
+        }
+
+        outputJoltage += numberString.toLong()
+    }
+    return outputJoltage
+}
+
+fun findLargestDigitWithAtLeastNDigitsRemaining(digits: String, startIndex: Int, charactersRemaining: Int): DigitAtIndex {
+    var largestDigit = 0
+    var index = 0
+
+    for(i in startIndex .. (digits.lastIndex - charactersRemaining) ) {
+        if(digits[i].digitToInt() > largestDigit) {
+            largestDigit = digits[i].digitToInt()
+            index = i
+        }
+    }
+
+    return DigitAtIndex(digits[index], index)
 }
